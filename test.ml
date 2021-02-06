a65988f5aa39c19625491ede430016a4ea85f169
Niols <niols@niols.fr>
2021-02-06 16:38:55 +0100

    more testing

--- test.ml
+++ test.ml
@@ -4,3 +4,13 @@ with
 | 1 -> print_endline "Got error 1"; Ok ()
 | 2 -> print_endline "Got error 1"; Ok ()
 ;;
+
+let foo =
+  let%res e = assert%result (1 = 2) in
+  let%res x = assert%result false in
+  Ok ()
+;;
+
+match foo with
+| Ok x -> print_endline "Got OK."
+| Error x -> print_endline "Got error."; raise x
