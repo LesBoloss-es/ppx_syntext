d797d6d76c86f2eb6941d3df7feda994277dad5f
Niols <niols@niols.fr>
2021-02-10 16:54:07 +0100

    sanitise variables in option & result

--- src/plugins/option/ppx_syntext_option.ml
+++ src/plugins/option/ppx_syntext_option.ml
@@ -1,17 +1,35 @@
 open Migrate_parsetree
 open OCaml_411.Ast
 
-let on_return x = [%expr Some [%e x]]
+let on_return x =
+  [%expr Some [%e x]]
 
-let on_bind o f = [%expr match [%e o] with Some x -> [%e f] x | None -> None]
+let on_bind o f =
+  [%expr
+    match [%e o] with
+    | Some syntext_var_x -> [%e f] syntext_var_x
+    | None -> None]
 
-let on_return_error x = [%expr ignore [%e x]; None]
+let on_return_error x =
+  [%expr
+    ignore [%e x];
+    None]
 
-let on_bind_error o f = [%expr match [%e o] with Some x -> Some x | None -> [%e f] ()]
+let on_bind_error o f =
+  [%expr
+    match [%e o] with
+    | Some syntext_var_x -> Some syntext_var_x
+    | None -> [%e f] ()]
 
-let on_assert_false () = [%expr None]
+let on_assert_false () =
+  [%expr None]
 
-let on_assert e = [%expr if [%e e] then Some () else None]
+let on_assert e =
+  [%expr
+    if [%e e] then
+      Some ()
+    else
+      None]
 
 let applies_on = "opt(ion)?"
 
--- src/plugins/result/ppx_syntext_result.ml
+++ src/plugins/result/ppx_syntext_result.ml
@@ -1,17 +1,38 @@
 open Migrate_parsetree
 open OCaml_411.Ast
 
-let on_return x = [%expr Ok [%e x]]
+let on_return x =
+  [%expr Ok [%e x]]
 
-let on_bind r f = [%expr match [%e r] with Ok x -> [%e f] x | Error e -> Error e]
+let on_bind r f =
+  [%expr
+    match [%e r] with
+    | Ok syntext_var_x -> [%e f] x
+    | Error syntext_var_e -> Error syntext_var_e]
 
-let on_return_error e = [%expr Error [%e e]]
+let on_return_error e =
+  [%expr Error [%e e]]
 
-let on_bind_error r f = [%expr match [%e r] with Ok x -> Ok x | Error e -> [%e f] e]
+let on_bind_error r f =
+  [%expr
+    match [%e r] with
+    | Ok syntext_var_x -> Ok syntext_var_x
+    | Error syntext_var_e -> [%e f] syntext_var_e]
 
-let on_assert_false () = [%expr try assert false with exn -> Error exn]
+let on_assert_false () =
+  (* It is not necessary to sanitise the variable here, but we do it for
+     consistency. Better safe than sorry. *)
+  [%expr
+    try
+      assert false
+    with
+    | syntext_var_exn -> Error syntext_var_exn]
 
-let on_assert e = [%expr if [%e e] then Ok () else [%e on_assert_false ()]]
+let on_assert e =
+  [%expr
+    if [%e e] then
+      Ok ()
+    else [%e on_assert_false ()]]
 
 let () = Ppx_syntext.(register (
     create "result.ok" ~applies_on:"ok|res(ult)?(.ok)?"
