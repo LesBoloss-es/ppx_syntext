0469be3b70b33bfc0555c1bdffa505442fccf869
Niols <niols@niols.fr>
2021-02-11 11:51:43 +0100

    fix ppx_nop_monad: add bind_error and return_error

--- test/nop/ppx_nop/ppx_nop_monad.ml
+++ test/nop/ppx_nop/ppx_nop_monad.ml
@@ -1,9 +1,26 @@
 open Migrate_parsetree
 open OCaml_411.Ast
 
+let on_bind e f =
+  [%expr [%e f] [%e e]]
+
+let on_return e =
+  e
+
+let on_bind_error e f =
+  let pexn, exn = Ppx_syntext.Helpers.fresh_variable () in
+  [%expr
+    try
+      [%e e]
+    with
+    | [%p pexn] -> [%e f] [%e exn]]
+
+let on_return_error e =
+  [%expr raise [%e e]]
+
 let () = Ppx_syntext.(register (
     create "ppx_nop_monad"
       ~applies_on:"nop"
-      ~on_bind:(fun e f -> [%expr [%e f] [%e e]])
-      ~on_return:(fun e -> e)
+      ~on_bind ~on_return
+      ~on_bind_error ~on_return_error
   ))
